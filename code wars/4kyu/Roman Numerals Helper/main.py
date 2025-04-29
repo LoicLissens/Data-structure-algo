d885d3eb5fb72a12d2b8512e226384af22be493a
Loic Lissens <loiclissens@gmail.com>
2025-04-29 16:31:58 +0200

    1+1=3
    Signed-off-by: Loic Lissens <loiclissens@gmail.com>

---
+++ code wars/4kyu/Roman Numerals Helper/main.py
@@ -0,0 +1,11 @@
+class RomanNumerals:
+    symb_val_mapping = {
+        
+    }
+    @staticmethod
+    def to_roman(val : int) -> str:
+        return ''
+
+    @staticmethod
+    def from_roman(roman_num : str) -> int:
+        return 0
