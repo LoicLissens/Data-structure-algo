2d6e88819669698b640f4491dffb6360f5c5dca0
Loic Lissens <loiclissens@gmail.com>
2025-04-30 21:15:13 +0200

    vie 🖖
    Signed-off-by: Loic Lissens <loiclissens@gmail.com>

--- code wars/4kyu/Roman Numerals Helper/main.py
+++ code wars/4kyu/Roman Numerals Helper/main.py
@@ -1,11 +1,44 @@
 class RomanNumerals:
-    symb_val_mapping = {
-        
-    }
-    @staticmethod
-    def to_roman(val : int) -> str:
-        return ''
+    mapper = {
+            1000: "M",
+            900: "CM",
+            500: "D",
+            400: "CD",
+            100: "C",
+            90: "XC",
+            50: "L",
+            40: "XL",
+            10: "X",
+            9: "IX",
+            5: "V",
+            4: "IV",
+            1: "I",
+        }
+    @classmethod
+    def to_roman(cls,val: int) -> str:
+        roman_str = ""
+        for key in sorted(cls.mapper.keys(), reverse=True):
+            while val >= key:
+                roman_str += cls.mapper[key]
+                val -= key
+        return roman_str
 
-    @staticmethod
-    def from_roman(roman_num : str) -> int:
-        return 0
+    @classmethod
+    def from_roman(cls,roman_str: str) -> int:
+        roman_int = 0
+        for k,v in cls.mapper.items():
+            if len(v) < 2:
+                continue
+            if v in roman_str:
+                roman_int += k
+                roman_str = roman_str.replace(v, "")
+
+        for k,v in cls.mapper.items():
+            while v in roman_str:
+                roman_int += k
+                roman_str = roman_str.replace(v, "",1)
+
+        return roman_int
+
+# print(RomanNumerals.to_roman(1989))
+print(RomanNumerals.to_roman())
